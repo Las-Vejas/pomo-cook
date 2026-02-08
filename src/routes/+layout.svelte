42f3e93c40e0f4658023319f5942df7edf830270
vejas <118309632+Las-Vejas@users.noreply.github.com>
2026-02-08 21:26:23 +0100

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { inject } from '@vercel/analytics';
+
+	inject();
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
+
